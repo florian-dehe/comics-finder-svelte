d1839afc34d3c20f468e1e13a1b6bab925366b17
Florian Dehe <florian.dehe@gmail.com>
2023-06-04 00:09:33 +0200

    DB: Changed comic.desc to Text & formatted code

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-    import { firebaseAuth } from "$lib/firebase";
-	import { signInWithEmailAndPassword } from "firebase/auth";
-    import { authStore } from "$lib/stores/auth";
-    
-    let email: string;
-    let password: string;
-    let incorrect: boolean;    
-    
-    authStore.subscribe(async ({ isLoggedIn }) => {
-        if (isLoggedIn) {
-            goto("/");
-        }
-    });
+	import { goto } from '$app/navigation';
+	import { firebaseAuth } from '$lib/firebase';
+	import { signInWithEmailAndPassword } from 'firebase/auth';
+	import { authStore } from '$lib/stores/auth';
 
-    const handleLogin = () => {
-        signInWithEmailAndPassword(firebaseAuth, email, password)
-            .then( () => {
-                goto('/');
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage);
+	let email: string;
+	let password: string;
+	let incorrect: boolean;
 
-                incorrect = true;
-            });
-    };
+	authStore.subscribe(async ({ isLoggedIn }) => {
+		if (isLoggedIn) {
+			goto('/');
+		}
+	});
+
+	const handleLogin = () => {
+		signInWithEmailAndPassword(firebaseAuth, email, password)
+			.then(() => {
+				goto('/');
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log(errorCode, errorMessage);
+
+				incorrect = true;
+			});
+	};
 </script>
 
 <div class="h-screen flex flex-col justify-center">
@@ -39,35 +39,35 @@
 					<p class="mt-3 text-red-500 text-center font-semibold">Wrong credentials</p>
 				{/if}
 
-                <form on:submit|preventDefault={handleLogin}>
-                    <div>
-                        <label for="id_email" class="label">
-                            <span class="label-text">Email</span>
-                        </label>
-                        <input
-                            type="email"
-                            id="id_email"
-                            required
-                            class="input input-bordered w-full max-w-sm"
-                            bind:value={email}
-                        />
+				<form on:submit|preventDefault={handleLogin}>
+					<div>
+						<label for="id_email" class="label">
+							<span class="label-text">Email</span>
+						</label>
+						<input
+							type="email"
+							id="id_email"
+							required
+							class="input input-bordered w-full max-w-sm"
+							bind:value={email}
+						/>
 
-                        <label for="id_password" class="label">
-                            <span class="label-text">Password</span>
-                        </label>
-                        <input
-                            type="password"
-                            id="id_password"
-                            required
-                            class="input input-bordered w-full max-w-sm"
-                            bind:value={password}
-                        />
+						<label for="id_password" class="label">
+							<span class="label-text">Password</span>
+						</label>
+						<input
+							type="password"
+							id="id_password"
+							required
+							class="input input-bordered w-full max-w-sm"
+							bind:value={password}
+						/>
 
-                        <div class="flex flex-row justify-center my-5">
-                            <input type="submit" value="Sign In" class="btn btn-primary" />
-                        </div>
-                    </div>
-                </form>
+						<div class="flex flex-row justify-center my-5">
+							<input type="submit" value="Sign In" class="btn btn-primary" />
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
--- src/routes/(auth)/logout/+page.svelte
+++ src/routes/(auth)/logout/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { firebaseAuth } from "$lib/firebase";
-    import { signOut } from "firebase/auth";
-    import { authStore } from "$lib/stores/auth";
-    
-    const logout = () => {
-        signOut(firebaseAuth)
-          .catch((error: Error) => {
-            console.error(error);
-          });
-    };
+	import { goto } from '$app/navigation';
+	import { firebaseAuth } from '$lib/firebase';
+	import { signOut } from 'firebase/auth';
+	import { authStore } from '$lib/stores/auth';
 
-    if ($authStore.currentUser) {
+	const logout = () => {
+		signOut(firebaseAuth).catch((error: Error) => {
+			console.error(error);
+		});
+	};
+
+	if ($authStore.currentUser) {
 		logout();
 		goto('/');
 	}
